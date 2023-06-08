74c164a035fb61ac2c083a321fee575ba946e9cd
XintongTheCoder <xintongmi@gmail.com>
2023-06-08 10:13:09 -0700

    serve client

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import connect from './connect';
 import { router } from './routes';
 
 dotenv.config();
 
 export const app: Application = express();
-
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // app.get('/', (req: Request, res: Response) => {
-//   res.send('Express + TypeScript Server');
+//   res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 // });
 
 app.use('/api/pandas', router);
